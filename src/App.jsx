4de6e34379329b871d6dfc16a1bbdb0a152feb14
Victor Zottmann <zottmann.victor@gmail.com>
2024-03-25 13:39:53 +1100

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import "./App.css";
 import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
     </div>
   );
 }
