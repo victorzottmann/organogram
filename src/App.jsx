68b9bbcb08b1c0f56b933a748648c15da6818964
Victor Zottmann <zottmann.victor@gmail.com>
2024-03-25 13:43:10 +1100

    change Banner to Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Banner from "./components/Banner";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <Banner/>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import banner from "../../assets/banner.png";
+
+const HeaderContainer = styled.header`
+  text-align: center;
+  background-color: #7B8EF9;
+`;
+
+const Banner = styled.img`
+  max-width: 100%;
+`;
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <Banner src={banner} alt="Banner of the OrganoGram website" />
+    </HeaderContainer>
+  );
+}
+
+export default Header;
